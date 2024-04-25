1e54a7ecc9e57c1019d03b97b4f63572cfe7f5d7
intitix <163463902+intitix@users.noreply.github.com>
2024-04-25 18:46:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -75,19 +75,7 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
-  if(map==NULL || key==NULL || value==NULL) return;
-  long pos = hash(key,map->capacity);
 
-  while(map->buckets[pos]!=NULL && map->buckets[pos]->key!=NULL) {
-      pos = (pos+1)%map->capacity;
-
-  }
-  map->buckets[pos] = createPair(key,value);
-  map->size++;
-
-  
-
-  
   return NULL;
 }
 
