6395de7b85147cb99890260c4158e1eaccad97c9
intitix <163463902+intitix@users.noreply.github.com>
2024-04-25 18:56:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -92,7 +92,15 @@ Pair *searchMap(HashMap *map, char *key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
+    map->current = -1;
+    for (long i = 0; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL) {
+            map->current = i;
+            return map->buckets[i];
+        }
+      
+    }
+    
     return NULL;
 }
 
